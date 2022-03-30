04cfe8c09f8f662daf0fbb2b9c47c3db2d7b9d0d
Carlos Barros <carlosbarros@pop-os.localdomain>
2022-03-30 16:21:45 -0300

    test: check if getAll return an array an array that is empty

--- test/unit/models/productsModels.js
+++ test/unit/models/productsModels.js
@@ -1,4 +1,7 @@
 const { expect } = require('chai');
+const sinon = require('sinon');
+
+const connection = require('../../../models/connection');
 const ProductModel = require('../../../models/productModel');
 
 describe('Verifica se ao chamar "getAll" de "ProductModel" ela possuí o comportamento esperado:', () => {
@@ -6,5 +9,35 @@ describe('Verifica se ao chamar "getAll" de "ProductModel" ela possuí o comport
     it('Existe uma função getAll', () => {
         expect(typeof ProductModel.getAll).to.be.equal('function');
     });
+
+    describe('Ao buscar todos os produtos cadastrados no banco de dados', () => {
+        describe('Caso não tenha produtos cadastrados no banco de dados', () => {
+
+            before(() => {
+                sinon.stub(connection, 'execute').resolves([]);
+            });
+        
+            after(() => {
+                connection.execute.restore();
+            });
+
+            it('O retorno é um array', async () => {
+                const product = await ProductModel.getAll();
+                expect(product).to.be.an('array');
+            });
+
+            it('O retorno é um array vazio', async () => {
+                const product = await ProductModel.getAll();
+                expect(product).to.be.an('array');
+                expect(product).to.be.an('array').that.is.empty;
+            });
+
+        });
+
+        describe('Caso tenha produtos cadastrados no banco de dados', () => {
+
+        });
+
+        });
 
 }); 
