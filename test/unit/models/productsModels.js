d16732dce8eb77ab8788d20804f7b2babe78ab29
Carlos Barros <carlosbarros@pop-os.localdomain>
2022-03-30 17:49:46 -0300

    test: add case test to getAll when exists a product and check if exists a function getById

--- test/unit/models/productsModels.js
+++ test/unit/models/productsModels.js
@@ -29,7 +29,6 @@ describe('Verifica se ao chamar "getAll" de "ProductModel" ela possuí o comport
 
             it('O retorno é um array vazio', async () => {
                 const product = await ProductModel.getAll();
-                expect(product).to.be.an('array');
                 expect(product).to.be.an('array').that.is.empty;
             });
 
@@ -37,8 +36,51 @@ describe('Verifica se ao chamar "getAll" de "ProductModel" ela possuí o comport
 
         describe('Caso tenha produtos cadastrados no banco de dados', () => {
 
+            const expectResult = [
+                {   
+                    id: 1,
+                    name: "Portal Gun",
+                    quantity: 10
+                },
+                {
+                    "id": 2,
+                    "name": "Microverse Battery",
+                    "quantity": 5
+                },
+            ]
+
+            before(() => {
+                sinon.stub(connection, 'execute').resolves([expectResult, []]);
+            });
+        
+            after(() => {
+                connection.execute.restore();
+            });
+            
+            it('Verifica se o array retornado não esta vazio', async () => {
+                const product = await ProductModel.getAll();
+                expect(product).to.be.an('array').that.is.not.empty;
+            });
+
+            it('Verifica se o valor dentro do array é um objeto', async () => {
+                const [product] = await ProductModel.getAll();
+                expect(product).to.be.an('object');
+            });
+
+            it('Verifica se o produto dentro do array possui as chaves "id", "name", "quantity"', async () => {
+                const [product] = await ProductModel.getAll();
+                expect(product).to.have.all.keys('id', 'name', 'quantity');
+            });
         });
 
         });
-
 }); 
+
+describe('Verifica se ao chamar "getById" de "ProductModel" ela possuí o comportamento esperado:', () => {
+
+    it('Existe uma função getById', () => {
+        expect(typeof ProductModel.getById).to.be.equal('function');
+    });
+
+
+});
