a4c57047d947bab16e43da89c93a0b6fbeb2121f
Carlos Barros <carlosbarros@pop-os.localdomain>
2022-03-30 17:06:00 -0300

    feat:add getAll function

--- models/productModel.js
+++ models/productModel.js
@@ -1,4 +1,16 @@
+const connection = require('./connection');
+
 const getAll = async () => {
+
+const [products] = await connection.execute(
+    `
+    SELECT  *
+        FROM StoreManager.products
+        ORDER BY id;
+    `
+);
+
+return products;
 
 }
 
