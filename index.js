fdaf121c611f7b299096601c2bc499114c7be431
Carlos Barros <carlosbarros@pop-os.localdomain>
2022-03-30 17:03:29 -0300

    feat:add app with express and temporary route to test productModel

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 require('dotenv').config();
+const express = require('express');
+const ProductModel = require('./models/productModel');
+const app = express();
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.get('/products', async (_request, response) => {
+  const products = await ProductModel.getAll();
+
+  return response.status(200).json(products);
+
+
 });
 
 app.listen(process.env.PORT, () => {
