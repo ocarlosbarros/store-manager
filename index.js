385999c8be101098fdf79d02815530fd48400dd1
Carlos Barros <carlosbarros@pop-os.localdomain>
2022-03-30 17:47:32 -0300

    feat:add middleware express.json

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const ProductModel = require('./models/productModel');
 const app = express();
 
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
@@ -12,6 +14,15 @@ app.get('/products', async (_request, response) => {
   const products = await ProductModel.getAll();
 
   return response.status(200).json(products);
+
+
+});
+
+app.get('/products/:id', async (request, response) => {
+  const { id } = request.params;
+  const product = await ProductModel.getById(id);
+
+  return response.status(200).json(product);
 
 
 });
