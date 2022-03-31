0fe69a739eeff6cc8e6a4114712caeac33a73cd1
Carlos Barros <carlosbarros@pop-os.localdomain>
2022-03-31 15:49:00 -0300

    feat:create functions getAll and getById in productController

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,1 +1,29 @@
-// Renomeie esse arquivo
+const ProductModel = require('../models/productModel');
+
+
+const getAll = async (_request, response, next) => {
+    try {
+
+        const products = await ProductModel.getAll();
+        return response.status(200).json(products);
+
+    } catch (error) {
+
+        next(error);
+    }
+
+  
+};
+
+const getById = async (request, response) => {
+    const { id } = request.params;
+    const product = await ProductModel.getById(id);
+  
+    return response.status(200).json(product);
+};
+
+
+module.exports = {
+    getAll,
+    getById,
+}
