8985d9b1fe99ea99ce759cc03a06f66d22dca878
Carlos Barros <carlosbarros@pop-os.localdomain>
2022-03-31 15:28:42 -0300

    feat:add routes in index.js

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
 require('dotenv').config();
 const express = require('express');
-const ProductModel = require('./models/productModel');
+const routes = require('./routes');
+
 const app = express();
 
 app.use(express.json());
+app.use(routes);
+
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-app.get('/products', async (_request, response) => {
-  const products = await ProductModel.getAll();
-
-  return response.status(200).json(products);
-
-
-});
-
-app.get('/products/:id', async (request, response) => {
-  const { id } = request.params;
-  const product = await ProductModel.getById(id);
-
-  return response.status(200).json(product);
-
-
-});
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
