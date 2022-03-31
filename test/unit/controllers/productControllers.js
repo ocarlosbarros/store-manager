f8f293fff9b2da3449ef9478b06faa0dd778e2c9
Carlos Barros <carlosbarros@pop-os.localdomain>
2022-03-31 15:47:44 -0300

    test:create test to getAll in controller

--- test/unit/controllers/productControllers.js
+++ test/unit/controllers/productControllers.js
@@ -12,12 +12,30 @@ describe('Verifica se ao chamar "getAll" de "ProductController" ela possuí o co
     });
 
     describe('Ao buscar todos os produtos cadastrados no banco de dados', () => {
-        describe('Caso não tenha produtos cadastrados no banco de dados', () => {
+        
+        const response = {};
+        const request = {};
 
+        before(() => {
+            response.status = sinon.stub()
+            .returns(response);
+            response.json = sinon.stub()
+            .returns(response);
+            sinon.stub(ProductModel, 'getAll').resolves([]);
         });
 
-        describe('Caso tenha produtos cadastrados no banco de dados', () => {
+        after(() => {
+            ProductModel.getAll.restore();
+        });
 
+        it('Verifica se response.status 200', async () => {
+            await ProductController.getAll(request, response);
+            expect(response.status.calledWith(200)).to.be.equal(true);
+        });
+
+        it('Verifica se response.json retorna um array', async () => {
+            await ProductController.getAll(request, response);
+            expect(response.json.calledWith(sinon.match.array)).to.be.equal(true);
         });
     });
 
