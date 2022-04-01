54244a552c57ba25ccc925233f6624e8e2338a0b
Carlos Barros <carlosbarros@pop-os.localdomain>
2022-04-01 15:00:03 -0300

    test:refactor tests

--- test/unit/controllers/productControllers.js
+++ test/unit/controllers/productControllers.js
@@ -3,58 +3,64 @@ const sinon = require('sinon');
 
 const ProductController = require('../../../controllers/productController');
 const ProductModel = require('../../../models/productModel');
+const ProductService = require('../../../services/productService');
 
 
 describe('Verifica se ao chamar "getAll" de "ProductController" ela possuí o comportamento esperado:', () => {
-
     it('Existe uma função getAll', () => {
         expect(typeof ProductController.getAll).to.be.equal('function');
     });
 
-    describe('Ao buscar todos os produtos cadastrados no banco de dados', () => {
-        
-        const response = {};
-        const request = {};
+    describe('Quando getAll de ProductService retorna um array de produtos', () => {
+        let request = {}, response = {}, next = {};
 
         before(() => {
-            response.status = sinon.stub()
-            .returns(response);
-            response.json = sinon.stub()
-            .returns(response);
-            sinon.stub(ProductModel, 'getAll').resolves([]);
+            response.status = sinon.stub().returns(response);
+            response.json = sinon.stub().returns();
+            sinon.stub(ProductService, 'getAll').resolves([]);
         });
-
+    
         after(() => {
-            ProductModel.getAll.restore();
+            ProductService.getAll.restore();
         });
 
-        it('Verifica se response.status 200', async () => {
-            await ProductController.getAll(request, response);
+        it('responde a requisição com status', async () => {
+            await ProductController.getAll(request, response, next);
             expect(response.status.calledWith(200)).to.be.equal(true);
         });
 
-        it('Verifica se response.json retorna um array', async () => {
-            await ProductController.getAll(request, response);
+        it('retorna um array em response.json', async () => {
+            await ProductController.getAll(request, response, next);
             expect(response.json.calledWith(sinon.match.array)).to.be.equal(true);
         });
     });
 
-    describe('Acontece um erro ao chamar ProductService', () => {
+    describe('Quando getAll de ProductService retorna um erro', () => {
         let request = {}, response = {}, next = {};
         const error = Error('Erro ao processar o serviço');
-        
         before(() => {
             next = sinon.stub().returns();
-            sinon.stub(ProductModel, 'getAll').throws(error);
-          });
-      
-          after(() => {
-            ProductModel.getAll.restore();
-          });
+            sinon.stub(ProductService, 'getAll').throws(error);
+        });
+    
+        after(() => {
+            ProductService.getAll.restore();
+        });
 
-        it('Verifica se o "next" é chamado passando um Error', async () => {
+        it('next é chamado passando um Error', async () => {
             await ProductController.getAll(request, response, next);
             expect(next.calledWith(sinon.match(error))).to.be.equal(true);
         });
+
+    });
+});
+
+describe('Verifica se ao chamar "getById" de "ProductController" ela possuí o comportamento esperado:', () => {
+    
+    it('Existe uma função getById', () => {
+        expect(typeof ProductController.getById).to.be.equal('function');
+    });
+
+    describe('Ao charm', () => {
     });
 });
