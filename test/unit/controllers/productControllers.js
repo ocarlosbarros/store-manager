d4adb7c4febdaa12a450dedfea6c3a940b02ed6f
Carlos Barros <carlosbarros@pop-os.localdomain>
2022-04-01 18:08:43 -0300

    test:create tests when found and not found product by id

--- test/unit/controllers/productControllers.js
+++ test/unit/controllers/productControllers.js
@@ -2,7 +2,6 @@ const { expect } = require("chai");
 const sinon = require('sinon');
 
 const ProductController = require('../../../controllers/productController');
-const ProductModel = require('../../../models/productModel');
 const ProductService = require('../../../services/productService');
 
 
@@ -24,12 +23,12 @@ describe('Verifica se ao chamar "getAll" de "ProductController" ela possuí o co
             ProductService.getAll.restore();
         });
 
-        it('responde a requisição com status', async () => {
+        it('responde a requisição com status 200', async () => {
             await ProductController.getAll(request, response, next);
             expect(response.status.calledWith(200)).to.be.equal(true);
         });
 
-        it('retorna um array em response.json', async () => {
+        it('o método response.json retorna um array', async () => {
             await ProductController.getAll(request, response, next);
             expect(response.json.calledWith(sinon.match.array)).to.be.equal(true);
         });
@@ -38,6 +37,7 @@ describe('Verifica se ao chamar "getAll" de "ProductController" ela possuí o co
     describe('Quando getAll de ProductService retorna um erro', () => {
         let request = {}, response = {}, next = {};
         const error = Error('Erro ao processar o serviço');
+        
         before(() => {
             next = sinon.stub().returns();
             sinon.stub(ProductService, 'getAll').throws(error);
@@ -62,9 +62,60 @@ describe('Verifica se ao chamar "getById" de "ProductController" ela possuí o c
     });
 
     describe('Quando getById de ProductService retorna um produto', () => {
+        let request = {}, response = {}, next = {};
+
+        const productExpected = {
+            id: 1,
+            name: "Portal Gun",
+            quantity: 10
+        }
+
+        before(() => {
+            response.status = sinon.stub().returns(response);
+            response.json = sinon.stub().returns();
+            request.params = { id: 1 };
+            sinon.stub(ProductService, 'getById').resolves(productExpected);
+        });
+
+        after(() => {
+            ProductService.getById.restore();
+        });
+
+        it('responde a requisição com status 200', async () => {
+            await ProductController.getById(request, response, next);
+            expect(response.status.calledWith(200)).to.be.equal(true);
+        });
+
+        it('o método response.json é chamado passando um objeto', async () => {
+            await ProductController.getById(request, response, next);
+            expect(response.json.calledWith(sinon.match.object)).to.be.equal(true);
+        });
     });
 
-    describe('Quando getById de não retorna um produto', () => {
+    describe('Quando getById de ProductService não retorna um produto', () => {
+        let request = {}, response = {}, next = {};
+        const message = { message: "Product not found" };
+
+        before(() => {
+            response.status = sinon.stub().returns(response);
+            response.json = sinon.stub().returns();
+            request.params = { id: 1 };
+            sinon.stub(ProductService, 'getById').resolves(false);
+        });
+
+        after(() => {
+            ProductService.getById.restore();
+        });
+
+        it('responde a requisição com status 404', async () => {
+            await ProductController.getById(request, response, next);
+            expect(response.status.calledWith(404)).to.be.equal(true);
+        });
+
+        it('o método response.json é chamado passando a mensangem  "Product not found"', async () => {
+            await ProductController.getById(request, response, next);
+            expect(response.json.calledWith(message)).to.be.true;
+        });
     });
 
     describe('Quando getById de ProductService retorna um erro', () => {
