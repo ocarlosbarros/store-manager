09f0b8c47aecb7cfeb91461a49ca69301949a24e
Carlos Barros <carlosbarros@pop-os.localdomain>
2022-04-01 18:09:33 -0300

    refactor:change productModel by productService

--- controllers/productController.js
+++ controllers/productController.js
@@ -18,7 +18,10 @@ const getById = async (request, response, next) => {
     const { id } = request.params;
 
     try {
-        const product = await ProductModel.getById(id);
+        const product = await ProductService.getById(id);
+
+        if(!product) return response.status(404).json({ message: "Product not found"});
+        
         return response.status(200).json(product);
     } catch (error) {
         next(error);
