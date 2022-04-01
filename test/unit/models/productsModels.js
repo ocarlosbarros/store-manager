30fb98e31d6ae3c9a49e89f6cc512cedf73ce3cb
Carlos Barros <carlosbarros@pop-os.localdomain>
2022-04-01 14:53:32 -0300

    test:create tests to getById

--- test/unit/models/productsModels.js
+++ test/unit/models/productsModels.js
@@ -94,7 +94,7 @@ describe('Verifica se ao chamar "getById" de "ProductModel" ela possuí o compor
             });
             
             it('Retorna false caso não encontre o produto buscado pelo id informado', async () => {
-                const result = await ProductModel.getById(999);
+                const result = await ProductModel.getById();
                 expect(result).to.be.false;
             });
             
@@ -102,6 +102,31 @@ describe('Verifica se ao chamar "getById" de "ProductModel" ela possuí o compor
 
         describe('Caso encontre o produto cadastrado com o id informado', () => {
 
+            const productExpected = {
+            
+                id: 1,
+                name: "Portal Gun",
+                quantity: 10
+            }
+            
+
+            before(() => {
+                sinon.stub(connection, 'execute').resolves([[productExpected],[]]);
+            });
+
+            after(() => {
+                connection.execute.restore();
+            });
+
+            it('Verifica se o valor retornado é um objeto', async () => {
+                const [product] = await ProductModel.getById();
+                expect(product).to.be.an('object');
+            });
+
+            it('Verifica se o valor retornado é um objeto com as keys id, name, quantity', async () => {
+                const [product] = await ProductModel.getById(999);
+                expect(product).to.be.have.all.keys('id', 'name', 'quantity');
+            });
         });
     });
 });
