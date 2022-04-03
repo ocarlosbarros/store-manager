748aae912a6e6a90da0ceb493ab8fcd5d2c54893
Carlos Barros <carlosbarros@pop-os.localdomain>
2022-04-03 00:40:48 -0300

    test:create tests when founded sale by id

--- test/unit/services/saleServices.js
+++ test/unit/services/saleServices.js
@@ -54,13 +54,41 @@ describe('Verifica se ao chamar "getById" de "SaleService" ela possuí o comport
 
         it('Retorna false caso não encontre a venda buscada pelo id informado', async () => {
             const result = await SaleService.getById();
-            expect(result).to.be.equal(false);
+            expect(result).to.be.false;
         });
 
     });
 
     describe('Caso encontre a venda cadastrada com o id informado', () => {
 
+        const expectedSale = {
+            date: "2021-09-09T04:54:29.000Z",
+            productId: 1,
+            quantity: 2
+        }
+
+        before(() => {
+            sinon.stub(SaleModel, 'getById').resolves(expectedSale);
+        });
+
+        after(() => {
+            SaleModel.getById.restore();
+        });
+
+        it('Verifica se o valor retornado é um objeto', async () => {
+            const founded = await SaleService.getById(1);
+            expect(founded).to.be.an('object');
+        });
+
+        it('Verifica se o objeto não está vazio', async () => {
+            const founded = await SaleService.getById(1);
+            expect(founded).to.be.not.empty;
+        });
+
+        it('Verifica se o valor retornado é um objeto com as keys date, productId, quantity', async () => {
+            const founded = await SaleService.getById(1);
+            expect(founded).to.be.have.all.keys('date', 'productId', 'quantity');
+        });
         
     });
 });
