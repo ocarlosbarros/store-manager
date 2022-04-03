fec1c059f5e838120aba9ed59ba210e014117fea
Carlos Barros <carlosbarros@pop-os.localdomain>
2022-04-02 22:14:53 -0300

    feat:create getById in saleModel

--- models/saleModel.js
+++ models/saleModel.js
@@ -13,6 +13,22 @@ const getAll = async () => {
     return sales;
 }
 
+const getById = async (id) => {
+    const [sales] = await connection.execute(
+    `
+    SELECT S.id, S.date, SP.product_id, SP.quantity
+        FROM StoreManager.sales as S
+        INNER JOIN StoreManager.sales_products AS SP ON SP.sale_id = S.id
+            WHERE S.id = ? ORDER BY S.id, SP.product_id;
+    `,
+    [id])
+
+    if(sales.length === 0) return false;
+
+    return sales;
+};
+
 module.exports = {
     getAll,
+    getById,
 }
