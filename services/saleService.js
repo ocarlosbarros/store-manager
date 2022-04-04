c9ed55cef76a7d5c81f73c7cd405c4dca0262337
Carlos Barros <carlos_dbs@hotmail.com>
2022-04-04 18:42:02 -0300

    feat:create serialize function

--- services/saleService.js
+++ services/saleService.js
@@ -1,13 +1,33 @@
 const SaleModel = require('../models/saleModel');
 
+const serialize = (sale) => {
+    return {
+            saleId: sale.id,
+            date: sale.date,
+            productId: sale.product_id,
+            quantity: sale.quantity
+        }
+}
+
 const getAll = async () => {
     const sales = await SaleModel.getAll();
-    return sales;
+    const newSales = sales.map(serialize);
+
+    return newSales;
 }
 
 const getById = async (id) => {
     const founded = await SaleModel.getById(id);
-    return founded;
+    
+    if(!founded) return founded;
+
+    const newSale = founded.map((sale) => ({
+        date: sale.date,
+        productId: sale.product_id,
+        quantity: sale.quantity,
+    }));
+
+    return newSale;
 }
 
 module.exports = {
