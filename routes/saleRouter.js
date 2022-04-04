cf232208c024e948c4fd9d60c4159d1caab0ff12
Carlos Barros <carlosbarros@pop-os.localdomain>
2022-04-04 15:54:11 -0300

    feat:add route sales/:id

--- routes/saleRouter.js
+++ routes/saleRouter.js
@@ -2,5 +2,6 @@ const saleRouter = require('express').Router();
 const SaleController = require('../controllers/saleController');
 
 saleRouter.get('/', SaleController.getAll);
+saleRouter.get('/:id', SaleController.getById);
 
 module.exports = saleRouter;
