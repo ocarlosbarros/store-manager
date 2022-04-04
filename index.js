174eb993d19838c9a5595722731cd03a2e5ce14f
Carlos Barros <carlos_dbs@hotmail.com>
2022-04-04 19:01:28 -0300

    style:fix all lint problems

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
-
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
