0d993d153a972281736be99d42edf071e8e81979
Carlos Barros <carlos_dbs@hotmail.com>
2022-04-04 20:34:02 -0300

    feat:add serielize and all serialize in sales

--- services/saleService.js
+++ services/saleService.js
@@ -1,6 +1,6 @@
 const SaleModel = require('../models/saleModel');
 
-const serialize = (sale) => ({
+const allSerialize = (sale) => ({
             saleId: sale.id,
             date: sale.date,
             productId: sale.product_id,
@@ -9,21 +9,23 @@ const serialize = (sale) => ({
 
 const getAll = async () => {
     const sales = await SaleModel.getAll();
-    const newSales = sales.map(serialize);
+    const newSales = sales.map(allSerialize);
 
     return newSales;
 };
+
+const serialize = (sale) => ({
+    date: sale.date,
+    productId: sale.product_id,
+    quantity: sale.quantity,
+});
 
 const getById = async (id) => {
     const founded = await SaleModel.getById(id);
     
     if (!founded) return founded;
 
-    const newSale = founded.map((sale) => ({
-        date: sale.date,
-        productId: sale.product_id,
-        quantity: sale.quantity,
-    }));
+    const newSale = founded.map(serialize);
 
     return newSale;
 };
@@ -31,4 +33,6 @@ const getById = async (id) => {
 module.exports = {
     getAll,
     getById,
+    allSerialize,
+    serialize,
 };
