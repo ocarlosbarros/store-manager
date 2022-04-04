de8529680567999810696dbebc9162408afb8155
Carlos Barros <carlos_dbs@hotmail.com>
2022-04-04 19:02:40 -0300

    style:fix all lint problems

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -4,5 +4,4 @@ const ProductController = require('../controllers/productController');
 productRouter.get('/', ProductController.getAll);
 productRouter.get('/:id', ProductController.getById);
 
-
 module.exports = productRouter;
