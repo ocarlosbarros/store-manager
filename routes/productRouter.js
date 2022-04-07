aa7cca76f8201bfe1f598653de2f87be76fa552a
Carlos Barros <carlos_dbs@hotmail.com>
2022-04-07 10:55:44 -0300

    feat:add create route

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -2,6 +2,7 @@ const productRouter = require('express').Router();
 const ProductController = require('../controllers/productController');
 
 productRouter.get('/', ProductController.getAll);
+productRouter.post('/', ProductController.create);
 productRouter.get('/:id', ProductController.getById);
 productRouter.delete('/:id', ProductController.destroy);
 
