4162d75efae0380ddf764e48f054a1ebc81c6d54
Carlos Barros <carlos_dbs@hotmail.com>
2022-04-07 11:24:53 -0300

    feat:add validation when create new product.

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,8 +1,9 @@
 const productRouter = require('express').Router();
 const ProductController = require('../controllers/productController');
+const ProductValidation = require('../middleware/productValidation');
 
 productRouter.get('/', ProductController.getAll);
-productRouter.post('/', ProductController.create);
+productRouter.post('/', ProductValidation.validate, ProductController.create);
 productRouter.get('/:id', ProductController.getById);
 productRouter.delete('/:id', ProductController.destroy);
 
