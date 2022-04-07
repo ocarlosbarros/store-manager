24d167ce78372675a0e83177e0b0ac2822153350
Carlos Barros <carlos_dbs@hotmail.com>
2022-04-07 14:50:34 -0300

    feat:add create route to sales

--- routes/saleRouter.js
+++ routes/saleRouter.js
@@ -2,6 +2,7 @@ const saleRouter = require('express').Router();
 const SaleController = require('../controllers/saleController');
 
 saleRouter.get('/', SaleController.getAll);
+saleRouter.post('/', SaleController.create);
 saleRouter.get('/:id', SaleController.getById);
 saleRouter.delete('/:id', SaleController.destroy);
 
