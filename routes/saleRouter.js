ade74987512b51436370ffe5d23758f577cdeff0
Carlos Barros <carlos_dbs@hotmail.com>
2022-04-11 09:36:11 -0300

    feat: add validation in routes

--- routes/saleRouter.js
+++ routes/saleRouter.js
@@ -1,10 +1,11 @@
 const saleRouter = require('express').Router();
 const SaleController = require('../controllers/saleController');
+const SaleValidation = require('../middleware/saleValidation');
 
 saleRouter.get('/', SaleController.getAll);
-saleRouter.post('/', SaleController.create);
+saleRouter.post('/', SaleValidation.validate, SaleController.create);
 saleRouter.get('/:id', SaleController.getById);
 saleRouter.delete('/:id', SaleController.destroy);
-saleRouter.put('/:id', SaleController.update);
+saleRouter.put('/:id', SaleValidation.validate, SaleController.update);
 
 module.exports = saleRouter;
