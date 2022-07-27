a6e5c57f36472912adc2a71000fda95b4fd53aed
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-27 12:57:56 -0300

    refactor: changes root route

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.send();
-});
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const routes = require('express').Router();
 const ProductRouter = require('./productRouter');
 const SaleRouter = require('./saleRouter');
 
+routes.get('/', (_request, response) => {
+    return response.send('Welcome Store Manager API!');
+});
+
 routes.use('/products', ProductRouter);
 routes.use('/sales', SaleRouter);
 
